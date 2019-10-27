bf3e18f8a42aae165f1238633439efcf8d6e5b72
Dr-min998 <53601398+Dr-min998@users.noreply.github.com>
2019-10-27 13:43:37 +0800

    Update phone.js

--- js/phone.js
+++ js/phone.js
@@ -5,3 +5,17 @@ $(document).ready(function(){
 		});
 	},2100)
 });
+function jjj(index){
+	let nub=`nfc${index}`;
+	$('.nf-center').fadeOut('.5s',()=>{
+		$("."+nub).fadeIn();
+	});
+	if (index==3) {
+		$('.user1').show(()=>{
+			let date = new Date();
+			let hour=date.getHours();
+			let min=date.getMinutes();
+			$('#time1').text(hour+":"+min);
+		});
+	}
+}
