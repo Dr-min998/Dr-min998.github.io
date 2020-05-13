5a0fc7f91baca4647ee852e564a196f48cd9eaeb
Dr-min998 <53601398+Dr-min998@users.noreply.github.com>
2020-05-13 10:16:07 +0800

    Update new_file.js

--- js/new_file.js
+++ js/new_file.js
@@ -5,6 +5,9 @@ $(document).ready(function(){
 	setTimeout(()=>{
 		$('#body_div').show();
 	},3650);
+	setTimeout(()=>{
+		$('.pl_down').css("height","5rem");
+	},5050);
 });
 
 window.onscroll= function(){
@@ -12,7 +15,21 @@ window.onscroll= function(){
 	var scrollup = document.getElementById('get_top');
 	if(t>=180){
 		scrollup.style.display="block";
+		$('.pl_down').hide();
+		$('.bgi').hide();
 	}else{
 		scrollup.style.display="none";
+		$('.pl_down').show();
+		$('.bgi').show();
+	}
+}
+var flag01=0;
+function fly_page(){
+	if (flag01==0) {
+		$('#get_top span').show();
+		flag01=1;
+	} else{
+		$('#get_top span').hide();
+		flag01=0;
 	}
 }
